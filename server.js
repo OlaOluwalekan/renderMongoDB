a6665b8c2ee02dacd000e1328ed32295e87cb393
OlaOluwalekan <olalekanbello534@gmail.com>
2023-02-15 23:19:33 +0100

    amother changes

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ const connect = async () => {
   }
 }
 
+mongoose.connection.on('disconnected', () => {
+  console.log('mongoDB disconnected')
+})
+
 const port = process.env.PORT || 9000
 
 app.listen(port, async () => {
